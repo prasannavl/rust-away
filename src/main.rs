64f2e5fef41ad450d728f46777719b7acbd15948
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-08-14 12:02:57 +0530

    add: migrate all observations to comments, from the issue: https://github.com/rust-lang-nursery/rls-vscode/issues/120#issuecomment-322105875

--- src/main.rs
+++ src/main.rs
@@ -32,26 +32,38 @@ fn main() {
     let mut initial_scores: Vec<String> = std::vec::Vec::new();
     initial_scores.push("10".to_string());
 
+    // FIXME: autocomplete shows into_iter - but not iter(). It seems to be a partial autocomplete list.
+    // FIXME: Type i unresolved, and as such no auto-complete further (possibly due to the above)    
     let i = initial_scores.iter();
 
+    // FIXME: Unresolved type (possibly related again to the above).
     let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
 
     let n = rand::thread_rng().gen_range(1, 10);
 
     println!("Guess the number! ({})", n);
 
+    // FIXME: Type recognition works. But auto-complete is partial - methods like cmp not available.
     let mut guess = String::new();
+
     io::stdin().read_line(&mut guess)
         .expect("failed to read line");
 
+    // FIXME: No autocomplete for guess_number even though the type is explicitly provided.
     let guess_number: u32 = guess.trim().parse()
         .expect("not a number!");
+
+    // FIXME: guess.cmp should be available on autocomplete for 'guess', but it isn't. 
+    // guess.cmp
 
     match guess_number.cmp(&n) {
         Ordering::Less    => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
         Ordering::Equal   => println!("You win!"),
     }
+
+    // FIXME: Only displays test mod inside. All other types are missing. This is just an example. Most cargo package don't show up with proper auto-complete. Uncomment and see if autocomplete works.
+    // primal::
 
     println!("Your number: {}", guess);
     println!("Good bye!");
